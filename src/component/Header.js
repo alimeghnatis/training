6c8efdf722b2a6b37d3b046c456533c8080c6784
alimeghnatis <alimg.dev@gmail.com>
2024-06-23 20:54:14 +0330

    added the header via scss

---
+++ src/component/Header.js
@@ -0,0 +1,14 @@
+import './header.scss'
+
+function Header() {
+  return (
+    <div className="header">
+      <h1>react test project</h1>
+      <ul className="list">
+        <li>first ite of the list</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Header
