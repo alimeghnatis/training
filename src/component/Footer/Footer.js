e3785ec26902209e7a577d55ed7d4271dd1a4d02
alimeghnatis <alimg.dev@gmail.com>
2024-07-08 23:14:32 +0330

    footer initiation

---
+++ src/component/Footer/Footer.js
@@ -0,0 +1,37 @@
+import './footer.scss'
+
+function Footer() {
+  return (
+    <div class="footer">
+      <div className="menu">
+        <ul>
+          <li><a href="#">Hello menu</a></li>
+          <li><a href="#">Hello menu</a></li>
+          <li><a href="#">Hello menu</a></li>
+          <li><a href="#">Hello menu</a></li>
+          <li><a href="#">Hello menu</a></li>
+        </ul>
+      </div>
+      <div className="contact-us">
+        <ul>
+          <li><a href="#">contact us</a></li>
+          <li><a href="#">contact us</a></li>
+          <li><a href="#">contact us</a></li>
+          <li><a href="#">contact us</a></li>
+          <li><a href="#">contact us</a></li>
+        </ul>
+      </div>
+      <div className="explanation">
+        <ul>
+          <li><a href="#">explanation for you</a></li>
+          <li><a href="#">explanation for you</a></li>
+          <li><a href="#">explanation for you</a></li>
+          <li><a href="#">explanation for you</a></li>
+          <li><a href="#">explanation for you</a></li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
