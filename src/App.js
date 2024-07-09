940cd09379b1d1d8a3f7aef91087ff27e37d38cc
Ashkan1279 <4shkanesmaeili@gmail.com>
2024-07-10 00:00:09 +0330

    Add main

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import Header from './component/Header/Header.js'
+import Main from './component/Main/Main.js';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>hh</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Main />
     </div>
   );
 }
