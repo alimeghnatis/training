7e81e9c3b40136066759fd169be312b9ef4be871
Ashkan1279 <4shkanesmaeili@gmail.com>
2024-07-15 19:58:15 +0330

    Added cards section.

--- src/component/Main/Main.js
+++ src/component/Main/Main.js
@@ -3,18 +3,20 @@ import './main.scss';
 function Main() {
   return (
     <main class="main">
+
+      {/* lessons */}
       <div className="lessons">
-        <section className="lessons-section" id="html-review">
+        <section className="lessons-section green-smooth" id="html-review">
           <div className="lessons-left-content">
             <h1 className="lessons-left-title fs-3">HTML</h1>
             <p className="lessons-left-introduction">The language for building web pages</p>
-            <a className="lessons-left-link green" href="#">Learn HTML</a>
+            <a className="link green" href="#">Learn HTML</a>
             <br />
-            <a className="lessons-left-link light-yellow" href="#">Video Tutorial</a>
+            <a className="link light-yellow" href="#">Video Tutorial</a>
             <br />
-            <a className="lessons-left-link black" href="#">HTML Reference</a>
+            <a className="link black" href="#">HTML Reference</a>
             <br />
-            <a className="lessons-left-link pink" href="#">Get Certified</a>
+            <a className="link pink" href="#">Get Certified</a>
           </div>
           <div className="lessons-right-content">
             <div className="code-board-wrapper">
@@ -32,11 +34,11 @@ function Main() {
           <div className="lessons-left-content">
             <h1 className="lessons-left-title fs-3">CSS</h1>
             <p className="lessons-left-introduction">The language for styling web pages</p>
-            <a className="lessons-left-link green" href="#">Learn CSS</a>
+            <a className="link green" href="#">Learn CSS</a>
             <br />
-            <a className="lessons-left-link black" href="#">CSS Reference</a>
+            <a className="link black" href="#">CSS Reference</a>
             <br />
-            <a className="lessons-left-link pink" href="#">Get Certified</a>
+            <a className="link pink" href="#">Get Certified</a>
           </div>
           <div className="lessons-right-content">
             <div className="code-board-wrapper">
@@ -54,11 +56,11 @@ function Main() {
           <div className="lessons-left-content">
             <h1 className="lessons-left-title fs-4">JavaScript</h1>
             <p className="lessons-left-introduction">The language for programming web pages</p>
-            <a className="lessons-left-link green" href="#">Learn JavaScript</a>
+            <a className="link green" href="#">Learn JavaScript</a>
             <br />
-            <a className="lessons-left-link white" href="#">JavaScript Reference</a>
+            <a className="link white" href="#">JavaScript Reference</a>
             <br />
-            <a className="lessons-left-link pink" href="#">Get Certified</a>
+            <a className="link pink" href="#">Get Certified</a>
           </div>
           <div className="lessons-right-content">
             <div className="code-board-wrapper">
@@ -76,11 +78,11 @@ function Main() {
           <div className="lessons-left-content">
             <h1 className="lessons-left-title fs-5">Python</h1>
             <p className="lessons-left-introduction">A popular programming language</p>
-            <a className="lessons-left-link green" href="#">Learn Python</a>
+            <a className="link green" href="#">Learn Python</a>
             <br />
-            <a className="lessons-left-link black" href="#">Python Reference</a>
+            <a className="link black" href="#">Python Reference</a>
             <br />
-            <a className="lessons-left-link pink" href="#">Get Certified</a> 
+            <a className="link pink" href="#">Get Certified</a> 
           </div>
           <div className="lessons-right-content">
             <div className="code-board-wrapper">
@@ -98,11 +100,11 @@ function Main() {
           <div className="lessons-left-content">
             <h1 className="lessons-left-title fs-5">SQL</h1>
             <p className="lessons-left-introduction">A language for accessing databases</p>
-            <a className="lessons-left-link green" href="#">Learn SQL</a>
+            <a className="link green" href="#">Learn SQL</a>
             <br />
-            <a className="lessons-left-link black" href="#">SQL Reference</a>
+            <a className="link black" href="#">SQL Reference</a>
             <br />
-            <a className="lessons-left-link pink" href="#">Get Certified</a>  
+            <a className="link pink" href="#">Get Certified</a>  
           </div>
           <div className="lessons-right-content">
             <div className="code-board-wrapper">
@@ -117,6 +119,67 @@ function Main() {
           </div>
         </section>
       </div>
+      
+      {/* cards */}
+      <div className="cards black">
+        <div className="cards-container">
+          <div className="cards-section pink" id="php-card">
+            <div className="cards-content-wrapper">
+              <h2 className="cards-title">PHP</h2>
+              <div className="cards-introduction-wrapper">
+                <h5 className="cards-introduction">A web server programming language</h5>
+              </div>
+              <a className="link black" href="#">Learn PHP</a>
+            </div>
+          </div>
+          <div className="cards-section light-yellow" id="jQuary-card">
+            <div className="cards-content-wrapper">
+              <h2 className="cards-title">jQuary</h2>
+              <div className="cards-introduction-wrapper">
+                <h5 className="cards-introduction">A JS library for developing web pages</h5>
+              </div>
+              <a className="link black" href="#">Learn jQuary</a>
+            </div>
+          </div>
+          <div className="cards-section pink-smooth" id="java-card">
+            <div className="cards-content-wrapper">
+              <h2 className="cards-title">Java</h2>
+              <div className="cards-introduction-wrapper">
+                <h5 className="cards-introduction">A programming language</h5>
+              </div>
+              <a className="link black" href="#">Learn Java</a>
+            </div>
+          </div>
+          <div className="cards-section green-smooth" id="C++-card">
+            <div className="cards-content-wrapper">
+              <h2 className="cards-title">C++</h2>
+              <div className="cards-introduction-wrapper">
+                <h5 className="cards-introduction">A programming language</h5>
+              </div>
+              <a className="link black" href="#">Learn C++</a>
+            </div>
+          </div>
+          <div className="cards-section sky-blue" id="w3-card">
+            <div className="cards-content-wrapper">
+              <h2 className="cards-title">W3.CSS</h2>
+              <div className="cards-introduction-wrapper">
+                <h5 className="cards-introduction">A CSS framework for faster and better responsive web pages</h5>
+              </div>
+              <a className="link black" href="#">Learn W3.CSS</a>
+            </div>
+          </div>
+          <div className="cards-section light-blue" id="bootstrap-card">
+            <div className="cards-content-wrapper">
+              <h2 className="cards-title">Bootstrap</h2>
+              <div className="cards-introduction-wrapper">
+                <h5 className="cards-introduction">A CSS framework for designing better web pages</h5>
+              </div>
+              <a className="link black" href="#">Learn Bootstrap</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      
     </main>  
   )
 }
