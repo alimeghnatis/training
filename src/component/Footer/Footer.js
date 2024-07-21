9d7b36a1bd3f077c3f2af78daf2af1462a31c481
AminPanahi <khode.mohamadaminpanahi@gmail.com>
2024-07-22 03:03:46 +0330

    footer updated

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -12,7 +12,7 @@ function Footer() {
     
       <div className="up-menu">
         <div className="uper-link">
-        
+          <a className="upl-a" href="">W3</a>
         </div>
         <div className="uper-link">
           <a className="upl-a" href="#">SPACES</a>
@@ -37,64 +37,111 @@ function Footer() {
         <div className="midm-block">  
           <h5 className="mmb-title">Top Tutorials</h5>
           <a href="" className="ga-button">HTML Tutorial</a>
+          <br/>
           <a href="" className="ga-button">CSS Tutorial</a>
-          <a href="" className="ga-button">JavaScript Tutorial</a>
+          <br/>
+          <a href="" className="ga-button">JavaScript Tutorial</a>  
+          <br/>
           <a href="" className="ga-button">How To Tutorial</a>
+          <br/>
           <a href="" className="ga-button">SQL Tutorial</a>
+          <br/>
           <a href="" className="ga-button">Python Tutorial</a>
+          <br/>
           <a href="" className="ga-button">W3.CSS Tutorial</a>
+          <br/>
           <a href="" className="ga-button">Bootstrap Tutorial</a>
+          <br/>
           <a href="" className="ga-button">PHP Tutorial</a>
+          <br/>
           <a href="" className="ga-button">Java Tutorial</a>
+          <br/>
           <a href="" className="ga-button">C++ Tutorial</a>
+          <br/>
           <a href="" className="ga-button">jQuery Tutorial</a>
+          <br/>
         </div>
 
         <div className="midm-block">
           <h5 className="mmb-title">Top References</h5>
           <a href="" className="ga-button">HTML Refrence</a>
+          <br/>
           <a href="" className="ga-button">CSS Refrence</a>
+          <br/>
           <a href="" className="ga-button">JavaScript Refrence</a>
+          <br/>
           <a href="" className="ga-button">SQL Refrence</a>
+          <br/>
           <a href="" className="ga-button">Python Refrence</a>
+          <br/>
           <a href="" className="ga-button">W3.CSS Refrence</a>
+          <br/>
           <a href="" className="ga-button">Bootstrap Refrence</a>
+          <br/>
           <a href="" className="ga-button">PHP Refrence</a>
+          <br/>
           <a href="" className="ga-button">Java Refrence</a>
+          <br/>
           <a href="" className="ga-button">Angular Refrence</a>
+          <br/>
           <a href="" className="ga-button">jQuery Refrence</a>
+          <br/>
         </div>
 
         <div className="midm-block">
           <h5 className="mmb-title">Top Examples</h5>
           <a href="" className="ga-button">HTML Examples</a>
+          <br/>
           <a href="" className="ga-button">CSS Examples</a>
+          <br/>
           <a href="" className="ga-button">JavaScript Examples</a>
+          <br/>
           <a href="" className="ga-button">How To Examples</a>
+          <br/>
           <a href="" className="ga-button">SQL Examples</a>
+          <br/>
           <a href="" className="ga-button">Python Examples</a>
+          <br/>
           <a href="" className="ga-button">W3.CSS Examples</a>
+          <br/>
           <a href="" className="ga-button">Bootstrap Examples</a>
+          <br/>
           <a href="" className="ga-button">PHP Examples</a>
+          <br/>
           <a href="" className="ga-button">Java Examples</a>
+          <br/>
           <a href="" className="ga-button">XML Examples</a>
+          <br/>
           <a href="" className="ga-button">jQuery Examples</a>
+          <br/>
         </div>
 
         <div className="midm-block">
           <h5 className="mmb-title">Get Certified</h5>
           <a href="" className="ga-button">HTML Certified</a>
+          <br/>
           <a href="" className="ga-button">CSS Certified</a>
+          <br/>
           <a href="" className="ga-button">JavaScript Certified</a>
+          <br/>
           <a href="" className="ga-button">Front End Certified</a>
+          <br/>
           <a href="" className="ga-button">SQL Certified</a>
+          <br/>
           <a href="" className="ga-button">Python Certified</a>
+          <br/>
           <a href="" className="ga-button">PHP Certified</a>
+          <br/>
           <a href="" className="ga-button">jQuery Certified</a>
+          <br/>
           <a href="" className="ga-button">Java Certified</a>
+          <br/>
           <a href="" className="ga-button">C++ Certified</a>
+          <br/>
           <a href="" className="ga-button">C# Certified</a>
+          <br/>
           <a href="" className="ga-button">XML Certified</a>
+          <br/>
         </div>
 
       </div>
