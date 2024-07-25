c6adf3eeec47d712f62cfe9798457b53b36890e0
AminPanahi <khode.mohamadaminpanahi@gmail.com>
2024-07-25 14:47:58 +0330

    header updated

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,116 +1,32 @@
 import './header.scss'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope, faXmark } from '@fortawesome/free-solid-svg-icons'
+
 
 
 function Header() {  
   return (
-    <header className="header">
-      <div className="left-content">
-
-          <div className="w3s-image-wrapper">
-            <a className="logo-link" href="#">
-              <img className="w3s-logo" src={require('./../image/channels4_profile.jpg')} />
-            </a>
-          </div>
-         
-          <nav className="nav-menu">
-            <ul>
-              <li id="tutorial-btn"><a href="#" className="nav-link ml"> Tutorials
-                <img className="down-arrow" src={require('./../image/back-up-down-svgrepo-com.png')} alt="" /></a>
-              </li>
-            </ul>
-            <ul>
-              <li><a href="#" className="nav-link ml">Exercises
-                <img className="down-arrow" src={require('./../image/back-up-down-svgrepo-com.png')} alt="" /></a>
-              </li>
-            </ul>
-            <ul>
-              <li><a href="#" className="nav-link ml">Certificates
-                <img className="down-arrow" src={require('./../image/back-up-down-svgrepo-com.png')} alt="" /></a>
-              </li>
-            </ul>  
-            <ul>
-              <li><a href="#" className="nav-link ml">Services
-                <img className="down-arrow" src={require('./../image/back-up-down-svgrepo-com.png')} alt="" /></a>
-              </li>
-            </ul>  
-          </nav>
-
-          <div className="search-input-wrapper">
-            <input className="search-input" type="search" placeholder="Search..." />
-            <div className="search-icon-wrapper">
-              <img className="search-icon" src={require('./../../images/search-svgrepo-com.png')} alt="" />
-            </div>
-          </div>
-          <div className="l-d-mode-wrapper">
-            <img className="l-d-mode" src={require('./../../images/dark-theme-svgrepo-com.png')} alt="" />
-          </div>
+      <div className="header1">
+        <div className="logo">
+          <img className="logo-img" src= {require('../image/logo.png')} alt="" />
         </div>
-        <div className="right-content">
-          <div className="plus-item-wrapper">
-            <a className="plus-item item" href="#">
-              <img className="plus-item-logo logo-item" src={require('./../../images/stars-svgrepo-com.png')} alt="" />
-              Plus
-            </a>
-          </div>
-          <div className="spaces-item-wrapper ml2">
-            <a className="spaces-item item" href="#">
-              <img className="spaces-item-logo logo-item" src={require('./../../images/code-svgrepo-com.png')} alt="" />
-              Spaces
-            </a>
-          </div>
-          <div className="certified-item-wrapper ml2">
-            <a className="certified-item item" href="#">
-              <img className="certificate-logo logo-item" src={require('./../../images/shop-cart-svgrepo-com.png')} alt="" />
-              Get Certified
-            </a>
-          </div>
-          <div className="sign-log-link-wrapper">
-            <div className="sign">
-              <a className="sign-link ml3" href="#">Sign Up</a>
-            </div>
-            <div className="log">
-              <a className="log-link" href="#">Log in</a>
-            </div>
-          </div>
-        </div>
-      </header>
 
-      <main> 
-        <div className="nested-navigation-container nested_navigation_hidden" id="nested-navigation-container-id">
-          <div className="nested-navigation-container-close-btn" >
-            <button className="clbtn">
-            <FontAwesomeIcon icon={faXmark} />
-            </button>
-          </div>
-          <div className="nested-navigation-container-content">
-            <h1 className="content-h1">Tutorials</h1>
-            <div className="nested-navigation-container-content-data">
-              <div className="nested-navigation-item">
-                <h2>HTML and CSS</h2>
-                <a href="" className="a11"><span className="span1">Learn</span><span className="span2">HTML</span></a>
-                <a href="" className="a11"><span className="span1">Learn</span><span className="span2">CSS</span></a>
-                <a href="" className="a11"><span className="span1">Learn</span><span className="span2">RWD</span></a>
-              </div>
-              <div className="nested-navigation-item">
-                <h2 className="item-h2">JavaScript</h2>
-                <a href="" className="a11"><span className="span1">Learn</span><span className="span2">JavaScript</span></a>
-                <a href="" className="a11"><span className="span1">Learn</span><span className="span2">React</span></a>
-                <a href="" className="a11"><span className="span1">Learn</span><span className="span2">jQuery</span></a>
-              </div>
-              <div className="nested-navigation-item">
-                <h2>Backend</h2>
-                <a href="" className="a11"><span className="span1">Learn</span><span className="span2">Python</span></a>
-                <a href="" className="a11"><span className="span1">Learn</span><span className="span2">SQL</span></a>
-                <a href="" className="a11"><span className="span1">Learn</span><span className="span2">MySql</span></a>
-              </div>
-            </div>
-          </div>
+        <div className="menu-but">
+          <a href="" className="menu-but-link">Menu</a>
         </div>
-      </main>  
-      
-  )
+
+        <div className="search-but">
+          <img className="search-img" src={require('../image/search-svgrepo-com.png')}  alt="" />
+        </div>
+        
+        <div className="dark-but">
+          <img className="dark-img" src={require('../image/dark-theme-svgrepo-com.png')} alt="" />
+        </div>
+
+        <div className="login-but">
+          <a href="" className="login-but-link">Log in</a>
+        </div>
+        
+      </div>
+  );
 }
 
 
--- src/component/Header/script.js
+++
@@ -1,8 +0,0 @@
-
-const NestedNavigationContainer = document.getElementById("nested-navigation-container-id");
-let tutorialEI = document.getElementById("tutorial-btn");
-
-tutorialEI.addEventListener("click",()=>{
-  
-  NestedNavigationContainer.classList.toggle("nested_navigation_hidden")
-})
